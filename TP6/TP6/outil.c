b2122f2030b4faa8ee01a3e8906df5e26bed19a1
Arthur BARRE <arthur.barre@isen.yncrea.fr>
2020-12-09 13:31:36 +0100

    partie 1 finie

--- TP6/TP6/outil.c
+++ TP6/TP6/outil.c
@@ -1,5 +1,5 @@
-#include <stdio.h>   /* pour les entr�es-sorties */
-#include <string.h>  /* pour les manipulations de cha�nes de caract�res */
+#include <stdio.h>   /* pour les entrées-sorties */
+#include <string.h>  /* pour les manipulations de chaînes de caractères */
 #include <conio.h>
 #include <ctype.h>
 #include <stdlib.h>
@@ -9,36 +9,33 @@
 #define VERSION 3.0
 #define SQUELET
 /**************************************************************************/
-/* Compl�ter votre nom ici                                                */
-/*   Nom :                         Pr�nom :                               */
+/* Compléter votre nom ici                                                */
+/*   Nom :  Barré                      Prénom : Arthur             */
 /**************************************************************************/
 
 extern bool modif;
 
 
 /**********************************************************************/
-/*  Ajout d'un contact dans le r�pertoire stock� en m�moire           */
+/*  Ajout d'un contact dans le répertoire stocké en mémoire           */
 /**********************************************************************/
 
-int ajouter_un_contact_dans_rep(Repertoire *rep, Enregistrement enr)
+int ajouter_un_contact_dans_rep(Repertoire* rep, Enregistrement enr)
 {
 #ifdef IMPL_TAB
-	// compl�ter code ici pour tableau
-	int idx;
-
+	// compléter code ici pour tableau
 	if (rep->nb_elts < MAX_ENREG)
 	{
-		rep->tab[rep->nb_elts] = enr;
-		rep->nb_elts++;
-		return(OK);
-		
-
+		modif = true;
+		rep->tab[rep->nb_elts] = enr;    //si la condition est bien vérifiée, on ajoute le contact à la fin du tableau
+		rep->nb_elts++;   //on augmente le nbre de valeur
+		return (OK);
 	}
 	else {
 		return(ERROR);
 	}
 
-	
+
 #else
 #ifdef IMPL_LIST
 
@@ -53,17 +50,17 @@ int ajouter_un_contact_dans_rep(Repertoire *rep, Enregistrement enr)
 
 	}
 	else {
-			//
-			// compl�ter code ici pour Liste
-			//
-			//
-			//
+		//
+		// compléter code ici pour Liste
+		//
+		//
+		//
 
 	}
 
 
 #endif
-	
+
 #endif
 
 
@@ -71,19 +68,19 @@ int ajouter_un_contact_dans_rep(Repertoire *rep, Enregistrement enr)
 
 } /* fin ajout */
   /**********************************************************************/
-  /* supprime du r�pertoire l'enregistrement dont l'indice est donn� en */
-  /*   param�tre       et place modif = true                            */
+  /* supprime du répertoire l'enregistrement dont l'indice est donné en */
+  /*   paramètre       et place modif = true                            */
   /**********************************************************************/
 #ifdef IMPL_TAB
-void supprimer_un_contact_dans_rep(Repertoire *rep, int indice) {
+void supprimer_un_contact_dans_rep(Repertoire* rep, int indice) {
 
-	// compl�ter code ici pour tableau
+	// compléter code ici pour tableau
 	if (rep->nb_elts >= 1)		/* s'il y a au moins un element ds le tableau */
 	{						/* et que l'indice pointe a l'interieur */
-		for (int i = indice; i < rep->nb_elts -1; i++) {
+		for (int i = indice; i < rep->nb_elts - 1; i++) {   //on décale tout le tableau sur la gauche à partir de l'indice jusque nb_elts-1
 			rep->tab[i] = rep->tab[i + 1];
 		}
-		rep->nb_elts -= 1;		/* ds ts les cas, il y a un element en moins */
+		rep->nb_elts--;		/* ds ts les cas, il y a un element en moins */
 		modif = true;
 	}
 
@@ -93,34 +90,34 @@ void supprimer_un_contact_dans_rep(Repertoire *rep, int indice) {
 #else
 #ifdef IMPL_LIST
   /************************************************************************/
-  /* supprime du r�pertoire l'enregistrement contenu dans le maillon elem */
-  /*                   et fixe modif � vrai                              */
+  /* supprime du répertoire l'enregistrement contenu dans le maillon elem */
+  /*                   et fixe modif à vrai                              */
   /************************************************************************/
   // complet
 
-	int supprimer_un_contact_dans_rep_liste(Repertoire *rep, SingleLinkedListElem *elem) {
-	
-	int ret=DeleteLinkedListElem(rep->liste, elem);
+int supprimer_un_contact_dans_rep_liste(Repertoire* rep, SingleLinkedListElem* elem) {
+
+	int ret = DeleteLinkedListElem(rep->liste, elem);
 	if (ret == 1) {
 		rep->nb_elts--;
 		modif = true;
 	}
 
-	return (0); 
-	}
+	return (0);
+}
 #endif
 #endif
 
 
-  /**********************************************************************/
-  /*  fonction d'affichage d'un enregistrement sur une ligne � l'�cran  */
-  /* ex Dupont, Jean                 0320304050                         */
-  /**********************************************************************/
+/**********************************************************************/
+/*  fonction d'affichage d'un enregistrement sur une ligne à l'écran  */
+/* ex Dupont, Jean                 0320304050                         */
+/**********************************************************************/
 void affichage_enreg(Enregistrement enr)
 {
-	// code � compl�ter ici
-	printf("%s, %s                 %s\n", enr.nom, enr.prenom, enr.tel);
-
+	// code à compléter ici
+	printf("\n%s, %s                 %s\n", enr.nom, enr.prenom, enr.tel);
+	return;
 
 } /* fin affichage_enreg */
   /**********************************************************************/
@@ -130,8 +127,6 @@ void affichage_enreg(Enregistrement enr)
   /**********************************************************************/
 void affichage_enreg_frmt(Enregistrement enr)
 {
-	// code � compl�ter ici
-	// comme fonction affichage_enreg, mais avec pr�sentation align�es des infos
 	printf("| %s", enr.nom);
 	for (int i = strlen(enr.nom); i < MAX_NOM; i++)
 		printf(" ");
@@ -150,52 +145,60 @@ void affichage_enreg_frmt(Enregistrement enr)
   /**********************************************************************/
 bool est_sup(Enregistrement enr1, Enregistrement enr2)
 {
-	// code � compl�ter ici
-	for (int i = 0; i < MAX_NOM; i++) {
-		if (tolower(enr1.nom[i]) < tolower(enr2.nom[i])) return (false);  //tolower = minusculer , toupper =masjucule
-		if (tolower(enr1.nom[i]) > tolower(enr2.nom[i])) return (true);
-	}
-	for (int i = 0; i < MAX_NOM; i++) {
-		if (tolower(enr1.prenom[i]) < tolower(enr2.prenom[i])) return false;
-		if (tolower(enr1.prenom[i]) > tolower(enr2.prenom[i])) return true;
-	}
+	// code à compléter ici
+	char tmp1[MAX_NOM];                                             //on copie les noms et les prenoms dans des variables temporaires et on les passe en majuscule
+	strncpy_s(tmp1, _countof(tmp1), enr2.nom, _TRUNCATE);
+	_strupr_s(tmp1, strlen(tmp1) + 1);
+	char tmp2[MAX_NOM];
+	strncpy_s(tmp2, _countof(tmp2), enr1.nom, _TRUNCATE);
+	_strupr_s(tmp2, strlen(tmp2) + 1);
+	char tmp3[MAX_NOM];
+	strncpy_s(tmp3, _countof(tmp3), enr2.prenom, _TRUNCATE);
+	_strupr_s(tmp3, strlen(tmp3) + 1);
+	char tmp4[MAX_NOM];
+	strncpy_s(tmp4, _countof(tmp4), enr1.prenom, _TRUNCATE);
+	_strupr_s(tmp4, strlen(tmp4) + 1);
+
+
+	if (strcmp((tmp1), (tmp2)) < 0) return (true);   //si tmp1 est plus haut dans l'ordre alphabétique ont return true
+	if (strcmp((tmp1), (tmp2)) > 0) return (false);  //sinon on vérifie si elle est plus haute, si c'est le cas on return false
+
+	if (strcmp((tmp3), (tmp4)) < 0) return (true);   //on refait pareil qu'avant avec les prénoms si jamais les noms sont les mêmes
+	if (strcmp((tmp3), (tmp4)) > 0) return (false);
 
 	return(false);
 
 }
- 
+
 /*********************************************************************/
 /*   Tri Alphabetique du tableau d'enregistrements                   */
 /*********************************************************************/
 
-void trier(Repertoire *rep)
+void trier(Repertoire* rep)
 {
-
 
 #ifdef IMPL_TAB
 	// ajouter code ici pour tableau
-
-	int k;
-	Enregistrement tmp;
-	Enregistrement min;
-	for (int i = 0; i < rep->nb_elts - 1; i++) {
-		min = rep->tab[i];
-		for (int j = 0; j < rep->nb_elts; j++) {
-			if (est_sup(min, rep->tab[j])) {
-				k = j;   //on prend l'indice du contact pour l'échanger après
-				min = rep->tab[j];
-}
+	for (int j = 0; j < rep->nb_elts; j++) {
+		for (int i = 0; i < rep->nb_elts - 1 - j; i++) {
+			// si enr1 > enr2, echange des enr
+			//printf("%s < %s = %d\n", rep->tab[i].nom, rep->tab[i+1].nom, est_sup(*(rep->tab + i), *(rep->tab + i + 1)));
+			if (est_sup(rep->tab[i + 1], rep->tab[i]) == false) {
+				printf("ECHANGE %s %s\n", rep->tab[i].nom, rep->tab[i + 1].nom);
+				Enregistrement tmp = rep->tab[i + 1];
+				rep->tab[i + 1] = rep->tab[i];
+				rep->tab[i] = tmp;
+			}
 		}
-		tmp = rep->tab[i];
-		rep->tab[i] = min;
-		rep->tab[k] = tmp;
 	}
-	
+
+
+
 #else
 #ifdef IMPL_LIST
 	// ajouter code ici pour Liste
-	// rien � faire !
-	// la liste est toujours tri�e
+	// rien à faire !
+	// la liste est toujours triée
 #endif
 #endif
 
@@ -205,10 +208,10 @@ void trier(Repertoire *rep)
 } /* fin trier */
 
   /**********************************************************************/
-  /* recherche dans le r�pertoire d'un enregistrement correspondant au  */
-  /*   nom � partir de l'indice ind                                     */
-  /*   retourne l'indice de l'enregistrement correspondant au crit�re ou*/
-  /*   un entier n�gatif si la recherche est n�gative				    */
+  /* recherche dans le répertoire d'un enregistrement correspondant au  */
+  /*   nom à partir de l'indice ind                                     */
+  /*   retourne l'indice de l'enregistrement correspondant au critère ou*/
+  /*   un entier négatif si la recherche est négative				    */
   /**********************************************************************/
 
 int rechercher_nom(Repertoire* rep, char nom[], int ind)
@@ -221,81 +224,76 @@ int rechercher_nom(Repertoire* rep, char nom[], int ind)
 							/* tableau, afin de les convertir en majuscules et les comparer */
 	bool trouve = false;
 
+
 #ifdef IMPL_TAB
-
 	// ajouter code ici pour tableau
-	ind_fin = rep->nb_elts - 1;
-	strcpy_s(tmp_nom, _countof(tmp_nom), nom);    //on copie nom dans tmp_nom, et on le passe en majuscule
-	_strupr_s(tmp_nom, _countof(tmp_nom));
-	while (trouve == false) {  //on va comparer a chaque nom du répertoire 
-		strcpy_s(tmp_nom2, _countof(tmp_nom2), rep->tab[i].nom);   //on copie dans tmp_nom2 le nom du répertoire
-		_strupr_s(tmp_nom2, _countof(tmp_nom2));
-		if (strcmp(tmp_nom, tmp_nom2)) {   //on compare
-			trouve = true;   //si ce sont les mêmes on passe trouve en true 
-		}
-		if (i == ind_fin) {
-			return -1;  //si on est arrivé à la fin, et qu'on a pas trouvé on return -1
-		}
-		else i++;  //sinon on incrémente i
-	}
-	return i; //on return l'indice du nom trouvé
-}
+	ind_fin = rep->nb_elts;
+	strncpy_s(tmp_nom, _countof(tmp_nom), nom, _TRUNCATE);    //on copie nom dans tmp_nom, et on le passe en majuscule
+	_strupr_s(tmp_nom, strlen(tmp_nom) + 1);
+	while (trouve == false && i < ind_fin) {  //on va comparer a chaque nom du répertoire jusqu'au dernier
 
-	
+		strncpy_s(tmp_nom2, _countof(tmp_nom2), rep->tab[i].nom, _TRUNCATE);   //on copie dans tmp_nom2 le nom du répertoire et on le passe en maj
+		_strupr_s(tmp_nom2, strlen(tmp_nom2) + 1);
+		if (strcmp(tmp_nom, tmp_nom2) == 0) trouve = true; //comparaison de la chaine de charactere on return true si c'est le même
+		else i++;  //sinon on passe au suivant
+	}
+
+
+
 #else
 #ifdef IMPL_LIST
-							// ajouter code ici pour Liste
-	
+	// ajouter code ici pour Liste
+
 #endif
 #endif
 
- /* fin rechercher_nom */
+	return((trouve) ? i : -1);
+} /* fin rechercher_nom */
 
   /*********************************************************************/
-  /* Supprimer tous les caracteres non num�riques de la chaines        */
+  /* Supprimer tous les caracteres non numériques de la chaines        */
   /*********************************************************************/
-void compact(char *s)
+void compact(char* s)
 {
-	// compl�ter code ici
+	int i = 0;
 	int j = 0;
-	int taille = strlen(s);
-	for (int i = 0; i < taille; i++) {
-
-		if (isalnum(s[i])) {}
-		else {
+	while (s[i] != '\0') {  //on parcours toute la chaine
+		if (s[i] < 0x30 || s[i] > 0x39) {   //codage ascii de 0 à 9, si ça n'appartient pas à ça, on le supprime
 			j = i;
-			while (s[j + 1] != '\0') {
+			while (s[j + 1] != '\0') {    //on écrase la valeur en décalant tout vers la gauche
 				*(s + j) = *(s + j + 1);
 				j++;
 			}
 		}
+		else i++;
 	}
 	return;
 }
 
 /**********************************************************************/
-/* sauvegarde le r�pertoire dans le fichier dont le nom est pass� en  */
+/* sauvegarde le répertoire dans le fichier dont le nom est passé en  */
 /* argument                                                           */
-/* retourne OK si la sauvegarde a fonctionn� ou ERROR sinon           */
+/* retourne OK si la sauvegarde a fonctionné ou ERROR sinon           */
 /**********************************************************************/
-int sauvegarder(Repertoire *rep, char nom_fichier[])
-{	
+int sauvegarder(Repertoire* rep, char nom_fichier[])
+{
 	errno_t err;
-	FILE* fic_rep;     /* le fichier */
+	FILE* fic_rep;                    /* le fichier */
 #ifdef IMPL_TAB
-	// ajouter code ici pour tableau
-	err = fopen_s(&fic_rep, *nom_fichier, "w");
-	for (int i = 0; i < rep->nb_elts; i++) {
-		fputs(rep->tab[i].nom, fic_rep);
-		fputs(",", fic_rep);
-		fputs(rep->tab[i].prenom, fic_rep);
-		fputs(",", fic_rep);
-		fputs(rep->tab[i].tel, fic_rep);
+	if (fopen_s(&fic_rep, nom_fichier, "w") != 0 || fic_rep == NULL) {   //on vérifie que le fichier est ouvert
+		err = ERROR;
+		return err;
+	}
+	for (int i = 0; i < rep->nb_elts; i++) {      //pour tous les éléments du tableau
+		fprintf(fic_rep, "%s%c", rep->tab[i].nom, SEPARATEUR);           //on écrit dans le fichier les info du contact
+		fprintf(fic_rep, "%s%c", rep->tab[i].prenom, SEPARATEUR);
+		fprintf(fic_rep, "%s\n", rep->tab[i].tel);
 
-}
+	}
 	if (feof(fic_rep)) {
 		fclose(fic_rep);
 	}
+
 #else
 #ifdef IMPL_LIST
 	// ajouter code ici pour Liste
@@ -307,24 +305,24 @@ int sauvegarder(Repertoire *rep, char nom_fichier[])
 
 
   /**********************************************************************/
-  /*   charge dans le r�pertoire le contenu du fichier dont le nom est  */
-  /*   pass� en argument                                                */
-  /*   retourne OK si le chargement a fonctionn� et ERROR sinon         */
+  /*   charge dans le répertoire le contenu du fichier dont le nom est  */
+  /*   passé en argument                                                */
+  /*   retourne OK si le chargement a fonctionné et ERROR sinon         */
   /**********************************************************************/
 
-int charger(Repertoire *rep, char nom_fichier[])
+int charger(Repertoire* rep, char nom_fichier[])
 {
-	FILE *fic_rep;					/* le fichier */
+	FILE* fic_rep;					/* le fichier */
 	errno_t err;
 	int num_rec = 0;						/* index sur enregistrements */
 	int long_max_rec = sizeof(Enregistrement);
 	char buffer[sizeof(Enregistrement) + 1];
 	int idx = 0;
 
-	char *char_nw_line;
-	
+	char* char_nw_line;
+
 	_set_errno(0);
-	if ( ((err = fopen_s(&fic_rep, nom_fichier, "r")) != 0) || (fic_rep == NULL) )
+	if (((err = fopen_s(&fic_rep, nom_fichier, "r")) != 0) || (fic_rep == NULL))
 	{
 		return(err);
 	}
@@ -349,7 +347,7 @@ int charger(Repertoire *rep, char nom_fichier[])
 					{
 						idx++;
 						if (lire_champ_suivant(buffer, &idx, rep->tab[num_rec].tel, MAX_TEL, SEPARATEUR) == OK)
-							num_rec++;		/* element � priori correct, on le comptabilise */
+							num_rec++;		/* element à priori correct, on le comptabilise */
 					}
 				}
 #else
